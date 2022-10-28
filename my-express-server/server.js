a5bfcb5bfc9e3d472dd4872057a38301ba370f9f
muradshah <murad.hadi@gmail.com>
2022-10-29 01:02:58 +0500

    add routes

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+	res.send("This is homepage");
+});
+
+app.get("/contact", (req, res) => {
+	res.send("this is contact page");
+});
+
+app.get("/about", (req, res) => {
+	res.send("This is about route");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}`);
+});
